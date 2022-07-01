6294ac93af441ba8bd97f6ff8674c56817b6fc36
Lei Hao <xiayushan2003@gmail.com>
2022-07-01 05:51:11 +0000

    update files

--- portfolio/src/main/java/com/google/sps/servlets/ListContactServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ListContactServlet.java
@@ -14,21 +14,19 @@
 
 package com.google.sps.servlets;
 
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Query;
 import com.google.cloud.datastore.QueryResults;
 import com.google.cloud.datastore.StructuredQuery.OrderBy;
-import com.google.gson.Gson;
-import com.google.sps.data.Task;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /** Servlet responsible for listing tasks. */
 @WebServlet("/list-tasks")
